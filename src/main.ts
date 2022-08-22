7bb37f8772ec18d1ab12c44be6a3387034e0bfb3
M.Polverini <m.polverini@almaviva.it>
2022-08-22 10:27:01 +0200

    fix cors error

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://orooval.com'],
+    origin: [
+      'http://localhost:4200',
+      'https://orooval.com',
+      'https://www.orooval.com',
+    ],
     exposedHeaders: '*',
     allowedHeaders: '*',
     credentials: true,
